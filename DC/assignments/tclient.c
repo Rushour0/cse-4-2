7266e3af8f73649545abd4736926e2aa9935995a
Rushour0 <pataderushikesh@gmail.com>
2024-02-01 23:50:31 +0530

    update makefile

--- DC/assignments/tclient.c
+++ DC/assignments/tclient.c
@@ -6,35 +6,105 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <string.h>
-int main()
+#include <pthread.h>
+
+#define MAX_BUFFER_SIZE 80
+#define SERVER_IP "127.0.0.1"
+#define SERVER_PORT 9129
+
+void back_space(char *str)
 {
-    int sockfd;
-    int len;
-    struct sockaddr_in address;
-    int result, count;
-    char str[80], instr[80];
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = inet_addr("127.0.0.1");
-    address.sin_port = 9129;
-    len = sizeof(address);
-     printf("Connecting to server...\n");
-    printf("Server address: %s\n", inet_ntoa(address.sin_addr));
-    printf("Server port: %d\n", ntohs(address.sin_port));
-    printf("Socket file descriptor: %d\n", sockfd);
-    result = connect(sockfd, (struct sockaddr *)&address, len);
-    if (result == -1)
-    {
-        printf("error\n");
-        exit(1);
+    int len = strlen(str);
+    for (int i = 0; i < len; i++) printf("\b");
+    for (int i = 0; i < len; i++) printf(" ");
+    for (int i = 0; i < len; i++) printf("\b");
+}
+
+int createSocket() {
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1) {
+        perror("Socket creation error");
+        exit(EXIT_FAILURE);
     }
-    fflush(stdout);
-    printf("\nenter string :");
-    fgets(instr, 80, stdin);
-    write(sockfd, instr, strlen(instr) + 1);
-    count = read(sockfd, str, 20);
-    printf("received data from server\n");
-    printf("%s\n", str);
+    return sockfd;
+}
+
+void setupServerAddress(struct sockaddr_in *address) {
+    address->sin_family = AF_INET;
+    address->sin_addr.s_addr = inet_addr(SERVER_IP);
+    address->sin_port = htons(SERVER_PORT);
+}
+
+void connectToServer(int sockfd, struct sockaddr *address, socklen_t len) {
+    printf("Connecting to server...\n");
+    printf("Server address: %s\n", inet_ntoa(((struct sockaddr_in *)address)->sin_addr));
+    printf("Server port: %d\n", ntohs(((struct sockaddr_in *)address)->sin_port));
+    printf("Socket file descriptor: %d\n", sockfd);
+    if (connect(sockfd, address, len) == -1) {
+        perror("Connection error");
+        exit(EXIT_FAILURE);
+    }
+}
+
+void sendDataToClient(int sockfd, const char *data) {
+    write(sockfd, data, strlen(data) + 1);
+
+    if (strcmp(data, "exit") == 0) {
+        printf("Client requested to close connection\n");
+        exit(EXIT_SUCCESS);
+    }
+}
+
+void receiveDataFromServer(int sockfd, char *buffer, size_t bufferSize) {
+    int count = read(sockfd, buffer, bufferSize - 1);
+    if (count == 0) {
+        printf("Server closed connection\n");
+        exit(EXIT_SUCCESS);
+    } else if (count == -1) {
+        perror("Read error");
+        exit(EXIT_FAILURE);
+    }
+    buffer[count] = '\0'; // Null-terminate the received data
+}
+
+void *sendThread(void *arg) {
+    int sockfd = *((int *)arg);
+    char instr[MAX_BUFFER_SIZE];
+
+    while (1) {
+        printf("Enter message to send: ");
+        fgets(instr, MAX_BUFFER_SIZE, stdin);
+        instr[strcspn(instr, "\n")] = '\0'; // Remove the newline character from the input
+        sendDataToClient(sockfd, instr);
+    }
+}
+
+int main() {
+    int sockfd;
+    struct sockaddr_in address;
+    socklen_t len = sizeof(address);
+    char str[MAX_BUFFER_SIZE];
+
+    sockfd = createSocket();
+    setupServerAddress(&address);
+    connectToServer(sockfd, (struct sockaddr *)&address, len);
+
+    pthread_t sendThreadId;
+    pthread_create(&sendThreadId, NULL, sendThread, (void *)&sockfd);
+
+    while (1) {
+        receiveDataFromServer(sockfd, str, sizeof(str));
+
+        if (strcmp(str, "exit") == 0) {
+            printf("Server closed connection\n");
+            exit(EXIT_SUCCESS);
+        }
+        back_space("Enter message to send: ");
+        printf("Server: %s\n", str);
+        printf("Enter message to send: ");
+        fflush(stdout);
+    }
+
     close(sockfd);
-    exit(0);
+    exit(EXIT_SUCCESS);
 }
--- DC/assignments/tserver.c
+++ DC/assignments/tserver.c
@@ -7,48 +7,146 @@
 #include <unistd.h>
 #include <string.h>
 #include <pthread.h>
-void *server_thread(void *arg)
+
+#define MAX_BUFFER_SIZE 80
+#define SERVER_IP "127.0.0.1"
+#define SERVER_PORT 9129
+
+
+void back_space(char *str)
 {
-    int client_sockfd;
-    char instr[80], name[80];
-    int count = 0;
-    client_sockfd = (int)arg;
-    count = read(client_sockfd, instr, 20);
-    instr[count] = '\0';
-    printf("input from client %s\n", instr);
-    strcpy(name, "Thank you");
-    strcat(name, instr);
-    strcpy(instr, name);
-    write(client_sockfd, instr, strlen(instr) + 1);
+    int len = strlen(str);
+    for (int i = 0; i < len; i++) printf("\b");
+    for (int i = 0; i < len; i++) printf(" ");
+    for (int i = 0; i < len; i++) printf("\b");
+
+}
+
+// Function to handle client communication in a separate thread
+void *handle_client(void *arg)
+{
+    int client_sockfd = *((int *)arg);
+    char client_input[MAX_BUFFER_SIZE];
+
+    while (1)
+    {
+        // Receive data from the client
+        ssize_t bytes_received = read(client_sockfd, client_input, sizeof(client_input) - 1);
+        if (bytes_received == -1)
+        {
+            perror("Error reading from client");
+            break;
+        }
+        else if (bytes_received == 0)
+        {
+            perror("Client disconnected\n");
+            break;
+        }
+
+        client_input[bytes_received] = '\0';
+
+        if (strcmp(client_input, "exit") == 0)
+        {
+            printf("Client requested to close connection\n");
+            break;
+        }
+
+        back_space("Enter message to send: ");
+        printf("Client: %s\n", client_input);
+        printf("Enter message to send: ");
+        fflush(stdout);
+    }
+
     close(client_sockfd);
     pthread_exit(NULL);
 }
 
-int main()
+// Function to set up the server socket
+int setup_server_socket()
 {
-    int server_sockfd, client_sockfd;
-    int server_len, client_len;
-    struct sockaddr_in server_address;
-    struct sockaddr_in client_address;
-    pthread_t t;
-    server_sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    server_address.sin_family = AF_INET;
-    server_address.sin_addr.s_addr = inet_addr("127.0.0.1");
-    server_address.sin_port = 9129;
-    server_len = sizeof(server_address);
-    if (bind(server_sockfd, (struct sockaddr *)&server_address, server_len) == -1)
+    int server_sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_sockfd == -1)
     {
-        printf("server error");
-        exit(1);
+        perror("Error creating server socket");
+        exit(EXIT_FAILURE);
     }
-    printf("server waiting....\n");
-    listen(server_sockfd, 5);
+
+    struct sockaddr_in server_address;
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = inet_addr(SERVER_IP);
+    server_address.sin_port = htons(SERVER_PORT);
+
+    if (bind(server_sockfd, (struct sockaddr *)&server_address, sizeof(server_address)) == -1)
+    {
+        perror("Error binding server socket");
+        close(server_sockfd);
+        exit(EXIT_FAILURE);
+    }
+
+    return server_sockfd;
+}
+
+void sendDataToClient(int sockfd, const char *data)
+{
+    write(sockfd, data, strlen(data) + 1);
+}
+
+void *sendThread(void *arg)
+{
+    int sockfd = *((int *)arg);
+    char instr[MAX_BUFFER_SIZE];
+
     while (1)
     {
-        client_len = sizeof(client_address);
-        client_sockfd = accept(server_sockfd, (struct sockaddr *)&client_address, &client_len);
-        printf("created new client thread  %d", client_sockfd);
-        pthread_create(&t, NULL, server_thread, (void *)client_sockfd);
+        printf("Enter message to send: ");
+        fgets(instr, MAX_BUFFER_SIZE, stdin);
+        instr[strcspn(instr, "\n")] = '\0'; // Remove the newline character from the input
+        sendDataToClient(sockfd, instr);
     }
-    return 0;
+}
+
+int main()
+{
+    int server_sockfd = setup_server_socket();
+    listen(server_sockfd, 5);
+
+    while (1)
+    {
+        struct sockaddr_in client_address;
+        socklen_t client_len = sizeof(client_address);
+        int client_sockfd = accept(server_sockfd, (struct sockaddr *)&client_address, &client_len);
+
+        if (client_sockfd == -1)
+        {
+            perror("Error accepting client connection");
+            continue;
+        }
+
+        printf("Accepted new client connection on socket %d\n", client_sockfd);
+
+        // Create a thread to handle the client communication
+        pthread_t client_thread;
+        if (pthread_create(&client_thread, NULL, handle_client, (void *)&client_sockfd) != 0)
+        {
+            perror("Error creating client thread");
+            close(client_sockfd);
+        }
+        else
+        {
+            pthread_detach(client_thread); // Detach the thread to allow it to clean up after completion
+        }
+        pthread_t send_thread;
+        if (pthread_create(&send_thread, NULL, sendThread, (void *)&client_sockfd) != 0)
+        {
+            perror("Error creating send thread");
+            close(client_sockfd);
+        }
+        else
+        {
+            pthread_detach(send_thread); // Detach the thread to allow it to clean up after completion
+        }
+    }
+
+    close(server_sockfd);
+    exit(EXIT_SUCCESS);
 }
